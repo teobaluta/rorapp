f465080504419dd4af668873a360fc3c54294182
techclass <techclass@techclass-VirtualBox.(none)>
2013-07-21 14:10:21 +0300

    Ruby on rails app class.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'a5d012c6f7171b8c7d2a3aa395faab827853943add9ace31513b4bedf0d3b85d2f07761fc487a84d8bd0559ed92e1e7add42227c12bc794148fdd9085d4c61a3'
---
+++ test/unit/helpers/statis_pages_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class StatisPagesHelperTest < ActionView::TestCase
+end
